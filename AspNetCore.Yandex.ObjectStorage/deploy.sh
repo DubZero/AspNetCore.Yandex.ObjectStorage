e83cc752d329c44ff100b945d3b564af2924f600
Alexandr Shumkin <shumkinalexandr@outlook.com>
2019-07-02 15:53:19 +0400

    verbosity fix

--- AspNetCore.Yandex.ObjectStorage/deploy.sh
+++ AspNetCore.Yandex.ObjectStorage/deploy.sh
@@ -1,6 +1,6 @@
 ApiKey=$1
 Source=$2
 
-dotnet pack ./obj/Release/AspNetCore.Yandex.ObjectStorage.nuspec -Verbosity detailed
+dotnet pack ./obj/Release/AspNetCore.Yandex.ObjectStorage.nuspec -v detailed
 
-dotnet nuget push ./bin/Release/AspNetCore.Yandex.ObjectStorage.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+dotnet nuget push ./bin/Release/AspNetCore.Yandex.ObjectStorage.*.nupkg -v detailed -ApiKey $ApiKey -Source $Source
