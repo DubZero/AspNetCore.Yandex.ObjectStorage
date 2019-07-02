1e7fae461eb4a46221adadde8da4f7885dab9f9e
Alexandr Shumkin <shumkinalexandr@outlook.com>
2019-07-02 15:44:47 +0400

    nuget to dotnet

--- AspNetCore.Yandex.ObjectStorage/deploy.sh
+++ AspNetCore.Yandex.ObjectStorage/deploy.sh
@@ -1,6 +1,6 @@
 ApiKey=$1
 Source=$2
 
-nuget pack ./obj/Release/AspNetCore.Yandex.ObjectStorage.nuspec -Verbosity detailed
+dotnet pack ./obj/Release/AspNetCore.Yandex.ObjectStorage.nuspec -Verbosity detailed
 
-nuget push ./bin/Release/AspNetCore.Yandex.ObjectStorage.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+dotnet push ./bin/Release/AspNetCore.Yandex.ObjectStorage.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
