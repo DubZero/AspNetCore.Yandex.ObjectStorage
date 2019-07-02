ef8515af9b3fe7322478ff2f500cb8371ae70ef1
Alexandr Shumkin <shumkinalexandr@outlook.com>
2019-07-02 15:39:09 +0400

    fix paths

--- AspNetCore.Yandex.ObjectStorage/deploy.sh
+++ AspNetCore.Yandex.ObjectStorage/deploy.sh
@@ -1,6 +1,6 @@
 ApiKey=$1
 Source=$2
 
-nuget pack ./AspNetCore.Yandex.ObjectStorage/AspNetCore.Yandex.ObjectStorage.nuspec -Verbosity detailed
+nuget pack ./obj/Release/AspNetCore.Yandex.ObjectStorage.nuspec -Verbosity detailed
 
-nuget push ./AspNetCore.Yandex.ObjectStorage.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+nuget push ./bin/Release/AspNetCore.Yandex.ObjectStorage.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
