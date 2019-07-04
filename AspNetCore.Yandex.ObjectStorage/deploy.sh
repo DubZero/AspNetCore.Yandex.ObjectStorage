adafaacedb31ecf746977c23a21574ca78f62195
Alexandr Shumkin <shumkinalexandr@outlook.com>
2019-07-04 11:53:32 +0400

    fix push

--- AspNetCore.Yandex.ObjectStorage/deploy.sh
+++ AspNetCore.Yandex.ObjectStorage/deploy.sh
@@ -1,6 +1,4 @@
 ApiKey=$1
 Source=$2
 
-dotnet pack ./obj/Release/AspNetCore.Yandex.ObjectStorage.nuspec -v detailed
-
-dotnet nuget push ./bin/Release/AspNetCore.Yandex.ObjectStorage.*.nupkg -v detailed -ApiKey $ApiKey -Source $Source
+dotnet nuget push ./bin/Release/AspNetCore.Yandex.ObjectStorage.*.nupkg -ApiKey $ApiKey -Source $Source
