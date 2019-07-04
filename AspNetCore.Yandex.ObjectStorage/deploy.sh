9d570d480229fcbbb34077a7bd3d7d282f77a84b
Alexandr Shumkin <shumkinalexandr@outlook.com>
2019-07-04 11:56:52 +0400

    fix keys

--- AspNetCore.Yandex.ObjectStorage/deploy.sh
+++ AspNetCore.Yandex.ObjectStorage/deploy.sh
@@ -1,4 +1,4 @@
 ApiKey=$1
 Source=$2
 
-dotnet nuget push ./bin/Release/AspNetCore.Yandex.ObjectStorage.*.nupkg -ApiKey $ApiKey -Source $Source
+dotnet nuget push ./bin/Release/AspNetCore.Yandex.ObjectStorage.*.nupkg --api-key $ApiKey --source $Source
