fa1f4a592db77240f053c379f72cb6d673253784
Alexandr Shumkin <shumkinalexandr@outlook.com>
2019-07-04 15:43:39 +0400

    fix setters for options

--- AspNetCore.Yandex.ObjectStorage/YandexStorageOptions.cs
+++ AspNetCore.Yandex.ObjectStorage/YandexStorageOptions.cs
@@ -2,13 +2,12 @@ namespace AspNetCore.Yandex.ObjectStorage
 {
     public class YandexStorageOptions
     {
-        public string Protocol { get; } = YandexStorageDefaults.Protocol;
-        public string BucketName { get; }
-        public string Location { get; } = YandexStorageDefaults.Location;
-        public string Endpoint { get; } = YandexStorageDefaults.EndPoint;
-        public string AccessKey { get; }
-        public string SecretKey { get; }
-
+        public string Protocol { get; set; } = YandexStorageDefaults.Protocol;
+        public string BucketName { get; set; }
+        public string Location { get; set; } = YandexStorageDefaults.Location;
+        public string Endpoint { get; set; } = YandexStorageDefaults.EndPoint;
+        public string AccessKey { get; set;}
+        public string SecretKey { get; set; }
         public string HostName => $"{Protocol}://{Endpoint}/{BucketName}";
     }
 }
